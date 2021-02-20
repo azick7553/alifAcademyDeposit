6ae49e279ecc1a5f14ab7c93a6d13f3c8bbf2705
azick <azick@DESKTOP-QVAAGM2>
2021-02-20 18:54:29 +0500

    init proj

---
+++ alifAcademyDeposit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alifAcademyDeposit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
